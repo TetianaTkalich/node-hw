241434c479e264309e0be910f37c37e68f4278db
TetianaTkalich <114866505+TetianaTkalich@users.noreply.github.com>
2023-07-09 08:17:57 +0300

    HW2

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
 const fs = require("fs/promises");
 const path = require("path");
-const shortid = require("shortid");
+const { nanoid } = require("nanoid");
 const contactsPath = path.join(__dirname, "db/contacts.json");
 
 const listContacts = async () => {
   const data = await fs.readFile(contactsPath);
-  const contacts = JSON.parse(data);
-  return contacts;
+  return JSON.parse(data);
 };
 
 const getContactById = async (contactId) => {
   const contacts = await listContacts();
-  const contact = contacts.find((item) => item.id === contactId);
-  if (!contact) {
-    return null;
-  }
-  return contact;
+  const result = contacts.find((item) => item.id === contactId);
+
+  return result || null;
 };
 
 const removeContact = async (contactId) => {
@@ -24,16 +21,20 @@ const removeContact = async (contactId) => {
   if (index === -1) {
     return null;
   }
-  const [removedContact] = contacts.splice(index, 1);
-  await fs.writeFile(contactsPath, JSON.stringify(contacts));
-  console.log(removedContact);
+  const [results] = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return results;
 };
 
-const addContact = async (data) => {
+const addContact = async (name, email, phone) => {
   const contacts = await listContacts();
-  const newContact = { id: shortid.generate(), ...data };
+  const newContact = {
+    id: nanoid(),
+    ...{ name, email, phone },
+  };
   contacts.push(newContact);
-  await fs.writeFile(contactsPath, JSON.stringify(contacts));
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+
   return newContact;
 };
 
